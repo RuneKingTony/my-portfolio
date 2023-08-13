497ea4b4675a7a98c0717df6324f85af26d2bc22
Anthony Nkwagit <anthonynkwa92@gmail.com>
2023-08-13 14:19:26 +0100

    Frontend

---
+++ src/container /About/About.jsx
@@ -0,0 +1,53 @@
+import React, {useEffect, useState} from 'react'
+import {motion} from 'framer-motion'
+import {AppWrap, MotionWrap} from '../../wrapper'
+import './About.scss'
+
+import { urlFor, client } from '../../client'
+
+
+const About = () => {
+
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+  const query = '*[_type == "abouts"]'
+   client.fetch(query)
+   .then((data) => setAbouts(data));
+  
+  }, []);
+  return (
+    <>
+    <h2 className='head-text'> 
+    <span>choose a frontend developer who will make your website the perfect match for your audience.</span>
+    <br/>
+     What are you waiting for?
+     <br/>
+    <span>Let's create something unforgettable together!"
+
+</span>
+    </h2>
+    <div className='app__profiles'>
+      {abouts.map((about, index) => (
+        <motion.div
+        whileInView={{ opacity: 1}}
+        whileHover={{ scale: 1.1 }}
+        transition={{ duratio: 0.5, type:'tween'}}
+        className="app__profile-item"
+        key={about.title + index}
+        >
+           <img src={urlFor(about.imgUrl)}alt={about.title}/>
+           <h2 className='bold-text' style={{marginTop: 20}}>{about.title}</h2>
+           <h2 className='bold-text' style={{marginTop: 10}}>{about.description}</h2>
+        </motion.div>
+      ))}
+    </div>
+    </>
+  )
+} 
+
+export default AppWrap (
+  MotionWrap(About, 'app__about'),
+   'about',
+   "app__whitebg"
+   );
